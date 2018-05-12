65dba994f4124c36afffa37ee80011d0430747c1
Cosebdd <Cosebdd@gmail.com>
2018-05-12 17:48:59 +0200

    List profile languages

---
+++ Langy.UI/LanguageProfileTitleConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using Langy.Core.Config;
+using Langy.Core.Model;
+
+namespace Langy.UI
+{
+    public class LanguageProfileTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is string profileName))
+                return null;
+            var profile = AppConfig.CurrentConfig.LanguageProfiles.Single(p => p.Name == profileName);
+            return $"{profile.Name} ({profile.LanguageTags.Aggregate((a, b) =>  a + "," + b)})";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
